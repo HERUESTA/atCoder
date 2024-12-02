146ccbf17bf339e2117b460d302c983dd506da9e
seki <102042736+HERUESTA@users.noreply.github.com>
2024-12-02 21:38:17 +0900

    初回コミット

---
+++ abc380/a/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * AtCoder用クラス
+ */
+public class Main {
+    public static void main(String[] args) {
+ 
+    Scanner sc = new Scanner(System.in);
+    
+    // 数字を取得
+    int num = sc.nextInt();
+
+    if (num == 12365) {
+        System.out.println("Yes");
+    }
+    }
+}
